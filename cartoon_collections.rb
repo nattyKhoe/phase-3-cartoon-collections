ad823832c57ef277fd71e21ffe985f7b38ae821a
Natasha <natasha.khoe@gmail.com>
2022-11-01 21:02:20 +1100

    Did the find_the_cheese method using intersection (&)

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
+end
+
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map do |planeteer|
+    "#{planeteer.capitalize()}!"
+  end
+end
+
+def long_planeteer_calls (words)
+  words.any?{|word| word.length>4}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese = list & cheese_types
+  if cheese.length == 0
+    nil
+  else
+    cheese[0]
+  end
 end
